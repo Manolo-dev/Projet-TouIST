0c05b1bb344557f353138094bae40827f16651e4
coco875 <59367621+coco875@users.noreply.github.com>
2022-11-27 22:07:39 +0100

    add exo 2

--- filter.py
+++ filter.py
@@ -3,12 +3,12 @@ import re
 with open("output.txt", "r") as f:
     all_lines = f.read()
 
-all_lines = re.sub(r"0 p\(\d,\d,\d,\w+\)\n", "", all_lines)
+all_lines = re.sub(r"0 \w+\(.+\)\n", "", all_lines)
 print(all_lines)
-all_lines = re.findall(r"p\((\d),(\d),(\d),(\w+)\)", all_lines)
+all_lines = re.findall(r"(\w+)\((.+)\)", all_lines)
 print(all_lines)
-sorted_lines = sorted(all_lines, key=lambda x: (x[0], x[1], x[2]))
+sorted_lines = sorted(all_lines, key=lambda x: x)
 print(sorted_lines)
 
 with open("output.txt", "w") as f:
-    f.write("".join([f"1 p({x[0]},{x[1]},{x[2]},{x[3]})\n" for x in sorted_lines]))
+    f.write("".join([f"1 {x[0]}({x[1]})\n" for x in sorted_lines]))
