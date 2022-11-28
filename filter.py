59646c7ec7ab8bf61e15fe0e84a8e8c5dec00932
coco875 <59367621+coco875@users.noreply.github.com>
2022-11-28 21:33:51 +0100

    some fix

--- filter.py
+++ filter.py
@@ -7,7 +7,7 @@ all_lines = re.sub(r"0 \w+\(.+\)\n", "", all_lines)
 print(all_lines)
 all_lines = re.findall(r"(\w+)\((.+)\)", all_lines)
 print(all_lines)
-sorted_lines = sorted(all_lines, key=lambda x: x)
+sorted_lines = sorted(all_lines, key=lambda x: (x[1], x[0]))
 print(sorted_lines)
 
 with open("output.txt", "w") as f:
