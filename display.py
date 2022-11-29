d077a86c82ff5d4cf767c08a1432982e3ef51641
Manolo-dev <sardo.manolo.dev@gmail.com>
2022-11-29 14:03:16 +0100

    display with TUI

--- display.py
+++ display.py
@@ -1,42 +1,42 @@
-import pygame
 import re
 
 with open('output.txt') as f:
     data = f.read()
 
 data = re.findall(r"1 p\((\d),(\d),(\d),(\w+)\)", data)
-print(data)
-# data format is [(state, x, y, color), ...]
-
-pygame.init()
-
-screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE)
-pygame.display.set_caption("Display")
-
-clock = pygame.time.Clock()
+colors = {
+    'vert': '\033[48;5;41m',
+    'orange': '\033[48;5;214m',
+    'bleu': '\033[48;5;75m',
+    'rouge': '\033[48;5;197m',
+    'rose': '\033[48;5;201m',
+    'vide': '\033[48;5;234m',
+}
 
 for i in range(len(data)):
-    if data[i][3] == "bleu":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (0, 0, 255))
-    elif data[i][3] == "rouge":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (255, 0, 0))
-    elif data[i][3] == "vert":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (0, 255, 0))
-    elif data[i][3] == "orange":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (255, 165, 0))
-    elif data[i][3] == "rose":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (255, 192, 203))
-    elif data[i][3] == "vide":
-        data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), (0, 0, 0))
+    data[i] = (int(data[i][0]), int(data[i][1]), int(data[i][2]), data[i][3])
 
-square_size = 20
-space_between_squares = 10
+j = [0, 0, 0]
+for i in range(len(data)):
+    if data[i][0] > j[0]:
+        j[0] = data[i][0]
+    if data[i][1] > j[1]:
+        j[1] = data[i][1]
+    if data[i][2] > j[2]:
+        j[2] = data[i][2]
+j = [j[0]+1, j[1], j[2]]
+result = [[['vide']*j[1] for i in range(j[2])] for k in range(j[0])]
 
-running = True
-while running:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+for e in data:
+    result[e[0]][e[2]-1][e[1]-1] = e[3]
+
+for i in range(len(result)):
+    s = result[i]
+    print("\n=== ÉTAPE", i, "===\n")
+    for t in s:
+        for e in t:
+            print(colors[e] + "  " + '\033[0m', '', end="")
+        print()t.type == pygame.QUIT:
             running = False
 
     screen.fill((0, 0, 0))
